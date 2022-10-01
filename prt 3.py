cb93f753a4904130b798624067cec07358d01fcb
Abhijeet shandilya <114105970+abhjeet1104@users.noreply.github.com>
2022-10-01 17:05:51 +0530

    attendence calc
    if attendence is more than 75% and assignment completed more than 70% OR the student is sports player then only the student is eligible for exam
    
    if you are sports player then you will get grace marks = +10

---
+++ prt 3.py
@@ -0,0 +1,15 @@
+attendance = 90 # mendatory
+assignment_submitted = 40 # mendatory
+sports_player = True # not mendatory
+
+print(f"attendence criteria met? : {attendance >= 75}")
+print(f"assignment criteria met? : {assignment_submitted >= 70}")
+print(f"sports criteria met? : {sports_player}")
+
+if attendance >= 75 and assignment_submitted >= 70:
+    if sports_player:
+        print("The student can get grace marks = +10")
+    else:
+        print("The student is eligible to appear in final exam but not extra grace marks")
+else:
+    print("The student is not eligible")
