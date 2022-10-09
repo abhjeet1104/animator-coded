4ae5165ab0238567e28a110881375d1f79f7b03e
Abhijeet shandilya <114105970+abhjeet1104@users.noreply.github.com>
2022-10-09 16:41:04 +0530

    stations

--- station.py
+++ station.py
@@ -13,3 +13,16 @@ while stations[current_station] != destination_station:
   print("-----")
 else:
   print(f"I have arrived at: {stations[current_station]}")  
+
+stations = ["Station 01", "Station 02", "Station 03", "Station 04"]
+
+current_station = 0
+destination_station = "Station 03"
+
+while stations[current_station] != destination_station:
+  print(f"current station is: {stations[current_station]}")
+  print(f"my destination is: {destination_station}")
+  print("Continue the journey I haven't reached the destination")
+  current_station = current_station + 1
+  print(f"Next station is: {stations[current_station]}")
+  print("-----")
