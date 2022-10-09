0da6a226a2f1c69fa6070dee6910d20ee1a46d05
Abhijeet shandilya <114105970+abhjeet1104@users.noreply.github.com>
2022-10-09 16:10:03 +0530

    cut off example

--- Repetive attempts.py
+++ Repetive attempts.py
@@ -3,9 +3,9 @@ cutoff = 400
 scores = [100, 200, 300, 399, 500]
 
 # I will keep on attempting until I succeed
-year = 0
+year = 0 # indexing in python starts with 0
 
 while scores[year] < cutoff:
-  print(f"")
+  print(f"Your score is: {scores[year]}, cutoff: {cutoff}")
   print("I will attempt next year")
-  year = year + 1
+  year = year + 1 # I am adding 1 to current year
