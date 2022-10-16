8f456beee76c147c4ea2a4122979b07fd213eaa9
Abhijeet shandilya <114105970+abhjeet1104@users.noreply.github.com>
2022-10-16 12:10:39 +0530

    play mp3 via python command..👌🔈🔉🔊

---
+++ play music using py command.py
@@ -0,0 +1,6 @@
+# import required module
+from playsound import playsound
+
+# for playing note.mp3 file
+playsound('01cdc465-7ebe-475d-8f9d-7217fe43c124.mp3')
+print('playing sound using playsound')
