1b7938af15026cbf887acc29eb7cacc543d888b1
Abhijeet shandilya <114105970+abhjeet1104@users.noreply.github.com>
2022-10-18 00:50:42 +0530

    nesting list in python
    method 2

---
+++ nesting 2.py
@@ -0,0 +1,9 @@
+amazon_cart = [["watch", 500], ["phone",18000], ["laptop" , 50000]]
+               total_cost = 0
+               
+               for i in range (len (amazon_cart)):
+                   #print (i)
+                   print (amazon_cart[i] [1])
+                   total_cost = total_cost+amazon_cart i] [1]
+
+                   print(total_cost)
