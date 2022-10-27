96226862bd191445d4ba8a541bb8b32983e7c770
Abhijeet shandilya <114105970+abhjeet1104@users.noreply.github.com>
2022-10-27 15:13:01 +0530

    merge mail

---
+++ merge mail.py
@@ -0,0 +1,13 @@
+with open ("name.txt", 'r' , encoding = 'utf -8') as names file
+           #open body.txt for reading
+           with open ("body.txt" 'r' encoding = 'utf -8')as body_file :
+           #read enire content of the body
+           body = body.file. read()
+           #iterate over names
+           for name in name.file :
+
+           mail= "hello"+name + body
+           #write the mail to individual files
+           with open (name.srip()+, ".txt" , 'w' encoding ='utf-8') as mail file
+
+                   mail_file.write (mail).
