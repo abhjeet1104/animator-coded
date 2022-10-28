5bd477cf30e6e639261d0f7a57d6c621b01ad9cf
Abhijeet shandilya <114105970+abhjeet1104@users.noreply.github.com>
2022-10-28 09:52:59 +0530

    prime no 2

--- prime no..py
+++ prime no..py
@@ -1,25 +1,15 @@
-# Program to check if a number is prime or not
+# Python program to display all the prime numbers within an interval
 
-num = 2
+lower = 900
+upper = 1000
 
-# To take input from the user
-#num = int(input("Enter a number: "))
+print("Prime numbers between", lower, "and", upper, "are:")
 
-# define a flag variable
-flag = False
-
-# prime numbers are greater than 1
-if num > 1:
-    # check for factors
-    for i in range(2, num):
-        if (num % i) == 0:
-            # if factor is found, set flag to True
-            flag = True
-            # break out of loop
-            break
-
-# check if flag is True
-if flag:
-    print(num, "is not a prime number")
-else:
-    print(num, "is a prime number")
+for num in range(lower, upper + 1):
+   # all prime numbers are greater than 1
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
